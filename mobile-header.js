867b094d949491a8fe7971413a29f59ade753f3f
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-01 15:58:17 +0800

    改hb

--- mobile-header.js
+++ mobile-header.js
@@ -1,5 +1,5 @@
 let hamburger = document.querySelector(".hb");
-let menu = document.querySelector(".main-nav");
+let menu = document.querySelector(".menu");
     hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('isactive');
     menu.classList.toggle('active');
