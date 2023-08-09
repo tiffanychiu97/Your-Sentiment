04c1124fe25b9599292288a5c508d286b734e2ff
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-09 10:23:42 +0800

    faq大字, cursor

--- sentiment_for_you.js
+++ sentiment_for_you.js
@@ -1,27 +1,27 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 5,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
-  });
+// $(document).ready(function(){
+//     $('.slider').slick({
+//         centerMode: true,
+//         centerPadding: '60px',
+//         slidesToShow: 5,
+//         responsive: [
+//           {
+//             breakpoint: 768,
+//             settings: {
+//               arrows: true,
+//               centerMode: true,
+//               centerPadding: '40px',
+//               slidesToShow: 3
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               arrows: true,
+//               centerMode: true,
+//               centerPadding: '40px',
+//               slidesToShow: 1
+//             }
+//           }
+//         ]
+//       });
+//   });
