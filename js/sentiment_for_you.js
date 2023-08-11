7fe0659a845d5b107ad587179f77ce542037215d
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-12 00:33:18 +0800

    index/devices/sfy微調

--- js/sentiment_for_you.js
+++ js/sentiment_for_you.js
@@ -2,13 +2,14 @@ $('.owl-carousel').owlCarousel({
     stagePadding: 50,
     loop:true,
     margin:50,
-    nav:true,
     responsive:{
         0:{
+            nav: false,
             items:1
         },
         600:{
-            items:3
+            items:3,
+            nav:true
         },
       //   1000:{
       //       items:5
