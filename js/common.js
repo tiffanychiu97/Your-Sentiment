d00d8a07a426ea3a0e3c4bd633297caf6a18f86f
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-13 01:20:44 +0800

    改

--- js/common.js
+++ js/common.js
@@ -2,3 +2,23 @@ let images = document.querySelectorAll('img.lazyload');
 lazyload(images);
 
 AOS.init();
+
+// $(document).ready(function() {
+    
+//     if (window.matchMedia("(max-width: 1055.33px)").matches) {
+      
+//       $("body").click(function(){
+//         $(".menu").hide();
+//       });
+      
+
+//       $(".hb").click(function(e){
+//         e.stopPropagation();
+//       });
+//     }
+//   });
+  
+  
+  
+  
+  
