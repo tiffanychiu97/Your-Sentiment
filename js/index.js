b1a7ac8b2fdc902bfb2c90a9808cde36518a928e
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-12 17:36:10 +0800

    snap

--- js/index.js
+++ js/index.js
@@ -2,23 +2,19 @@ gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.matchMedia({
 "(min-width: 200px)": () => {
-// let pinWrap = document.querySelector(".pin-wrap");
-// let pinWrapWidth = pinWrap.offsetWidth;
-// let horizontalScrollLength = pinWrapWidth - window.innerWidth;
 gsap.to(".pin-wrap", {
   x: -1550,
   scrollTrigger: {
-    trigger: "#sectionPin", //觸發的物件
-    ease: "none",
+    trigger: "#sectionPin", 
     start: "top 100", // (物件開始位置, 卷軸開始位置) top center bottom px
     end: "bottom 262", //(物件結束位置, 卷軸結束位置) , 也可以設卷軸捲動多少結束動畫(+=300)
-    pin: true, // 物件執行完動畫會跟著卷軸走，類似 fixed-top
-    scrub: true, // 物件動畫根據卷軸捲動程度跑
+    pin: true, 
+    scrub: true,
     snap: 0.35,
     // toggleClass: "active", //  class名稱 須為字串
-    markers: false // 顯示標記
+    markers: false 
+    }
+    });
   }
-});
-}
 });
 
