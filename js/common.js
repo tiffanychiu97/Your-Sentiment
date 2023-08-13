b2d0bf230b3245714510f8e4e512b3fac1a45448
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-13 18:05:04 +0800

    index navbar

--- js/common.js
+++ js/common.js
@@ -4,7 +4,16 @@ AOS.init();
 let images = document.querySelectorAll('img.lazyload');
 lazyload(images);
 
-
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 // $(document).ready(function() {
     
