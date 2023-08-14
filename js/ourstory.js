136d23d0bd3cac48be24e47fa595d7a9b46d5f37
tiffanychiu97 <t1427t1427@gmail.com>
2023-08-14 08:01:05 +0800

    ourstory

--- js/ourstory.js
+++ js/ourstory.js
@@ -22,3 +22,36 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+$(document).ready(function() {
+   const mediaQuery = window.matchMedia('(min-width: 1000px)');
+
+   function handleScroll() {
+     const scrollTop = $(window).scrollTop();
+     const fastTranslation = scrollTop * 0.1; 
+     const slowTranslation = scrollTop * 0.04;
+
+     $(".fast").css("transform", `translateZ(${fastTranslation}px) translateY(-${fastTranslation * 0.1}px)`);
+     $(".slow").css("transform", `translateZ(${slowTranslation}px) translateY(-${slowTranslation * 1}px)`);
+   }
+
+   // 初始載入時執行
+   handleScroll();
+
+   // 視窗捲動時執行
+   $(window).scroll(function() {
+     if (mediaQuery.matches) {
+       handleScroll();
+     }
+   });
+
+   // 視窗大小變化時執行
+   mediaQuery.addEventListener(function(e) {
+     if (e.matches) {
+       handleScroll();
+     } else {
+       $(".fast, .slow").css("transform", "none");
+     }
+   });
+
+});
